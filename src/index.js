63982a0c604d838e36a135eba16e79deb3e29976
Leo <leonidasgarcialescano@gmail.com>
2024-10-03 19:36:12 -0500

    aver

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   res.send(`Hello ${name}`);
 });
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
